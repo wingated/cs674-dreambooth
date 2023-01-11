675448556ed5ead6cb745316503f06c90f5bd041
David Wingate <wingated@gmail.com>
2023-01-11 16:35:24 -0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,8 +4,8 @@ import torch
 model_id = "./output_model"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-#prompt = "a photo of sks rubber duck at the beach"
-prompt = "PaperCut sks rubber duck at the beach"
+prompt = "a photo of sks rubber duck at the beach"
+#prompt = "PaperCut sks rubber duck at the beach"
 #prompt = "woolitize, sks rubber duck in bright green grass, sun in background, fluffy, daisies"
 
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
