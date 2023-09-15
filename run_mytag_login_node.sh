0c9611d7060db369653478fe9d575ba444749a65
Austin <austin.stone@teledyneflir.com>
2023-09-15 09:49:45 -0600

    Added nano and vim to Docker. Extended inference.py to not overwrite images. Changed run_mytag.sh to run_mytag_login_node.sh and run_mytag_compute_node.sh to separate different needs. Removed tricky whitespace from run scripts. Updated train_dreambooth.py to comply with the latest accelerate version. Added run_inference_compute_node.sh to simplify inference.

---
+++ run_mytag_login_node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# some helpful debugging options
+set -e
+set -u
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load jq zstd pigz parallel libnvidia-container enroot                                                                                                   
+
+CONTAINER_NAME="mycontainer"
+
+# Check if container already exists using enroot list
+if ! enroot list | grep -q "^${CONTAINER_NAME}\$"; then
+    enroot create --force --name $CONTAINER_NAME ${HOME}/mytag.sqsh
+fi
+
+# run a shell 
+# Ensures that the run_training doesn't try using cuda
+enroot start \
+       --mount /lustre/scratch/usr/${USER}:/home/${USER}/compute --rw \
+       --mount ${HOME}/hf_models:/app/hf_models \
+       --mount ${HOME}/output_model:/app/output_model \
+       $CONTAINER_NAME \
+       bash -c "export CUDA_VISIBLE_DEVICES='' && ./run_training.sh --max_train_steps=0"
